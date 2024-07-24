d20a58568dd0dde803d7fe4976717e466e4b58fc
nicolc11 <nicolaos.christofidis@phs.scot>
2024-07-24 15:51:26 +0100

    Updates.

--- R/access.R
+++ R/access.R
@@ -1,32 +1,8 @@
-#' Check access to lookups
-#'
-#' @description
-#' `have_access()` provides a convenient way to check that you have the
-#' correct access to the necessary folders.
-#'
-#' @returns
-#' `[logical(1)]` - `TRUE` if you have appropriate access and `FALSE` otherwise.
-#'
-#' @examples
-#' \dontrun{
-#' library(phslookups)
-#' if (have_access()) {
-#'   print("Success!")
-#' } else {
-#'   print("Maybe next time")
-#' }
-#' }
-#' @export
 have_access <- function() {
-  read_access_to_dir(get_lookups_dir())
+  file.access(get_lookups_dir(), 4) >= 0L
 }
 check_access <- function(fail_on_no_access = TRUE) {
-  if (have_access()) {
-    cli::cli_inform(c("v" = "You have the appropriate file permissions!"),
-      .frequency = "once",
-      .frequency_id = "lookup_access_inform"
-    )
-  } else {
+  if (!have_access()) {
     no_access_msg <- c(
       "x" = "You don't have the appropriate file permissions to {get_lookups_dir()}",
       "i" = "Please raise a ServiceNow request for access to the UNIX acute dataset"
@@ -37,7 +13,4 @@ check_access <- function(fail_on_no_access = TRUE) {
       cli::cli_warn(no_access_msg)
     }
   }
-}
-read_access_to_dir <- function(path) {
-  file.access(path, 4) >= 0L
 }
