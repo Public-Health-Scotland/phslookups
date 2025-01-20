8b0194b237c3e5cf717d692d6d91987f2aa12d42
James McMahon <james.mcmahon@phs.scot>
2025-01-20 14:25:08 +0000

    Remove old 'metadata'
    I initially included the variable lists as data in the package, but we have decided against this approach. Instead, we will read the metadata directly from disk using dedicated metadata files.

--- data-raw/hscp_locality_variables.R
+++
@@ -1,5 +0,0 @@
-## code to prepare `hscp_locality_variables` dataset goes here
-
-hscp_locality_variables <- names(get_hscp_locality())
-
-usethis::use_data(hscp_locality_variables, overwrite = TRUE)
