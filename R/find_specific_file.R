9b1f538a938a8b667950b3f3539f390da8c94c95
Moohan <5982260+Moohan@users.noreply.github.com>
2025-05-14 08:12:48 +0000

    Style code (GHA)

--- R/find_specific_file.R
+++ R/find_specific_file.R
@@ -1,20 +1,20 @@
 #' Find a specific version of a lookup file
 #'
 #' @description
-#' This function retrieves the file path for a specific version of a lookup 
-#' file based on the provided directory and lookup type. It validates the 
-#' existence of the file and throws an error if no matching file is found. 
-#' For lookup types requiring multiple versions (e.g., SIMD Postcode or 
-#' SIMD DataZone), `version` must be a named list or vector with the 
+#' This function retrieves the file path for a specific version of a lookup
+#' file based on the provided directory and lookup type. It validates the
+#' existence of the file and throws an error if no matching file is found.
+#' For lookup types requiring multiple versions (e.g., SIMD Postcode or
+#' SIMD DataZone), `version` must be a named list or vector with the
 #' appropriate names (e.g., `simd_version` and `postcode_version`).
 #'
-#' @param directory The base directory where lookup files are stored. 
+#' @param directory The base directory where lookup files are stored.
 #' The function searches here and this directory's "Archive" subdirectory.
-#' @param lookup_type A string specifying the type of lookup file to find. 
-#' Supported values include `"SPD"`, `"HSCP Locality"`, `"SIMD DataZone"`, 
+#' @param lookup_type A string specifying the type of lookup file to find.
+#' Supported values include `"SPD"`, `"HSCP Locality"`, `"SIMD DataZone"`,
 #' and `"SIMD Postcode"`.
-#' @param version A string defining the version to locate, or a 
-#' named list/vector for lookup types requiring multiple versions. 
+#' @param version A string defining the version to locate, or a
+#' named list/vector for lookup types requiring multiple versions.
 #' For example:
 #'   - `"YYYY_1"` for `lookup_type = "SPD"`
 #'   - `"YYYYMMDD"` for `lookup_type = "HSCP Locality"`
